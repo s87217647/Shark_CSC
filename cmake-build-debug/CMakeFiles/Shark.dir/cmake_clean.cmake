347d129b581262f613e2ff9ca5df3f5c4b50fa7a
s87217647 <s87217647@gmail.com>
2020-11-24 19:03:33 -0800

    Initial File

---
+++ cmake-build-debug/CMakeFiles/Shark.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shark.dir/main.cpp.obj"
+  "Shark.exe"
+  "Shark.exe.manifest"
+  "Shark.lib"
+  "Shark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
